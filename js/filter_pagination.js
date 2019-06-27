2b01d2a51906c18dd0870586fec2cd4ee12fc687
kevinleemoody <Kevin.moody98@gmail.com>
2019-06-26 23:31:13 -0400

    removed search bar

--- js/filter_pagination.js
+++ js/filter_pagination.js
@@ -8,52 +8,14 @@ const pageButtonsUl = document.createElement("ul");
 const pageButtonsDiv = document.createElement("div");
 const studentsPerPage = 10;
 
-
-//Seach through the list of students
-//adapted from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list
-
-const searchBar = document.getElementById("myInput")
-const pageHeader = document.querySelector(".page-header");
-pageHeader.appendChild(searchBar);
-searchBar.offsetRight;
-
-
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("pagination");
-    li = ul.getElementsByTagName("active");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
-
-
-
-
-
-
-
 //////////Append elements for pagination
 
 pageButtonsDiv.className = "pagination";
 pageButtonsDiv.appendChild(pageButtonsUl);
-// This broke the code
-// pageButtonsDiv.style.position = "fixed";
-// pageButtonsDiv.style.bottom = "0px";
-// // pageButtonsDiv.setAttribute("position","fixed")
-// // pageButtonsDiv.setAttribute("bottom","0px")
 page.appendChild(pageButtonsDiv);
 
-////////////The showPage Function
+////////////The showPage Function to hide all of the items in the
+///// list except for the 10 you want to show.
 
 const showPage = (list, button) => {
  //     Loop over items in the list parameter
@@ -77,21 +39,19 @@ const showPage = (list, button) => {
        hideStudentsDiv.style.display = "none";
   }
 };
-/* Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-   When each link is clicked, the showPage function displays the 
-   corresponding page (set of ten students), and highlights that page's 
-   link. For example, clicking the link to page 2 will display students 
-   11 through 20 and highlight button 2. */
 
-
-
+// Create the `appendPageLinks function` to generate, append, 
+//and add functionality to the pagination buttons. When each link is 
+//clicked, the showPage function displays the corresponding
+//page (set of ten students), and highlights that page's link.
 
 const appendPageLinks = studentList => {
   let numOfStudents = studentList.length;
   let numOfPages = Math.ceil(numOfStudents / studentsPerPage);
   let selectedButton = 1;
   pageButtonsUl.innerHTML = "";
+
+
 
   showPage(studentList, selectedButton);
 
